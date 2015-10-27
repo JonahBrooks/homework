92a40556fb559b12ebb7fb13c0c77ec43d031d91
JonahBrooks <brooks.jonah.c@gmail.com>
2015-10-26 18:34:51 -0700

    fmul almost works now, but I'm out of time. I'm going to pretty everything up and submit this.

--- cs472/p02/fops.c
+++ cs472/p02/fops.c
@@ -62,18 +62,21 @@ double fadd(double x, double y)
   // Add them using the ALU
   if(ysign == 0 && xsign == 1) // x is negative, y is positive
   {
-    rfrac = yfrac - xfrac;
+    
+    rfrac = yfrac -xfrac;
     if(xfrac > yfrac)
     {
       rsign = 1; // Result will be negative 
+      rfrac = (~rfrac)+1; // Two's complement and all that
     }
   }
   else if(xsign == 0 && ysign == 1) // y is negative, x is positive
   {
-    rfrac = xfrac - yfrac;
+    rfrac = xfrac -yfrac;
     if(yfrac > xfrac)
     {
       rsign = 1; // Result will be negative 
+      rfrac = (~rfrac)+1; // Two's complement and all that
     }
   }
   else //if(xsign == ysign) // both the same sign
@@ -87,12 +90,17 @@ double fadd(double x, double y)
      }
     rsign = xsign; // The sign won't change from addition of like-signed opperands
   }
+  // Detect if r is 0
+  if(rfrac == 0) // This includes the implicit 1
+  {
+    rexp = 0; // Set r to be 0
+  }
   
   // Normallize rfrac
   while ((rfrac & FRAC_MASK) > 0 && (rfrac & MANT_MASK) == 0) // While the MSBit is 0
   {
     rfrac = rfrac << 1;
-    rexp = rexp + 1;
+    rexp = rexp - 1;
   }   
   rfrac = rfrac & FRAC_MASK; // Drop the implicit 1
 
@@ -101,14 +109,14 @@ double fadd(double x, double y)
              ((rfrac << FRAC_SHIFT) & FRAC_MASK);
   toReturn = dandi.d;
   dandi2.d = toReturn;
-  printf("x:%lu (0x%016lx)\ny:%lu (0x%016lx)\nx+y:%lu (0x%016lx)\n",xfrac,xfrac,yfrac,yfrac,rfrac,rfrac);
-  printf("The double value %f (bit pattern 0x%02x%02x%02x%02x%02x%02x%02x%02x) represents the following:\n",
-          dandi2.d, dandi2.c[0], dandi2.c[1], dandi2.c[2], dandi2.c[3], 
-          dandi2.c[4], dandi2.c[5], dandi2.c[6], dandi2.c[7]); 
-  printf("sign: %d (0x%lx)\texponent: %lu (0x%lx) \tmantissa: %f (0x%lx)\n", 
-          rsign, ((dandi2.u & SIGN_MASK) >> SIGN_SHIFT), 
-          (rexp-EXP_BIAS), ((dandi2.u & EXP_MASK) >> EXP_SHIFT), 
-          ((double)rfrac/pow(2.0,54.0)), ((dandi2.u & FRAC_MASK) >> FRAC_SHIFT));
+  //printf("x:%lu (0x%016lx)\ny:%lu (0x%016lx)\nx+y:%lu (0x%016lx)\n",xfrac,xfrac,yfrac,yfrac,rfrac,rfrac);
+  //printf("The double value %f (bit pattern 0x%02x%02x%02x%02x%02x%02x%02x%02x) represents the following:\n",
+  //        dandi2.d, dandi2.c[0], dandi2.c[1], dandi2.c[2], dandi2.c[3], 
+  //        dandi2.c[4], dandi2.c[5], dandi2.c[6], dandi2.c[7]); 
+  //printf("sign: %d (0x%lx)\texponent: %lu (0x%lx) \tmantissa: %f (0x%lx)\n", 
+  //        rsign, ((dandi2.u & SIGN_MASK) >> SIGN_SHIFT), 
+  //        (rexp-EXP_BIAS), ((dandi2.u & EXP_MASK) >> EXP_SHIFT), 
+  //        ((double)rfrac/pow(2.0,54.0)), ((dandi2.u & FRAC_MASK) >> FRAC_SHIFT));
   
   return toReturn;
 }
@@ -147,6 +155,8 @@ double fsub(double x, double y)
 
 double fmul(double x, double y)
 {
+  // I have no idea why this occasionally doubles, quadrupals, etc the product
+  // But I'm out of time to work on it, so I'm going to call this good.
   double toReturn;
   
   unsigned long int xexp;
@@ -170,7 +180,97 @@ double fmul(double x, double y)
   yfrac = ((dandi2.u & FRAC_MASK) >> FRAC_SHIFT);
   ysign = (dandi2.u & SIGN_MASK) >> SIGN_SHIFT;
 
-  return 0.0;
+  // Detect if one or both are 0
+  if(yexp == 0 && yfrac == 0) // y is 0
+  {
+    return 0.0;
+  }
+  if(xexp == 0 && xfrac == 0) // x is 0
+  {
+    return 0.0;
+  }
+
+  // If not, add the implicit 1 to each mantissa
+  xfrac = (xfrac | MANT_MASK); 
+  yfrac = (yfrac | MANT_MASK); 
+
+    // Note: I don't think I need this for mult
+//  // Allign the two numbers together
+//  int expdiff;
+//  expdiff = xexp - yexp;
+//  if(xexp < yexp)
+//  {
+//    xfrac = xfrac >> abs(expdiff);
+//    xexp = yexp;
+//    rexp = yexp;
+//  }
+//  else
+//  {
+//    yfrac = yfrac >> abs(expdiff);
+//    yexp = xexp;
+//    rexp = xexp;
+//  }
+
+  int shiftsx, shiftsy;
+  shiftsx = 0;
+  shiftsy = 0;
+  // Shift both values all the way to the right
+  while (xfrac > 0 && (xfrac & 1) == 0 ) // While the LSBit is 0
+  {
+    xfrac = xfrac >> 1;
+    //xexp = xexp + 1;
+    shiftsx++;
+  }   
+  while (yfrac > 0 && (yfrac & 1) == 0 ) // While the LSBit is 0
+  {
+    yfrac = yfrac >> 1;
+    shiftsy++;
+  }
+  
+  if(xsign == ysign) // both the same sign
+  {
+    rsign = 0; // Same sign means positive product
+  }
+  else
+  {
+    rsign = 1; // Different signs mean negative product
+  }
+  
+  // Multiply them using the ALU
+  rfrac = (xfrac * yfrac);
+  rexp = yexp + xexp - EXP_BIAS + ((shiftsx < shiftsy) ? (shiftsx) : (shiftsy)); // Add the two exponents, accounting for them both being biased
+  // Shift and increase exponent for any carry-over
+//  while ((rfrac & (~(MANT_MASK & FRAC_MASK))) > 0) // While there are some 1's in bits 63:53
+//  {
+//  printf("x:%lu (0x%016lx)\ny:%lu (0x%016lx)\nx*y:%lu (0x%016lx)\n",xfrac,xfrac,yfrac,yfrac,rfrac,rfrac);
+//    
+//    rfrac = rfrac >> 1;
+//    rexp = rexp + 1;
+//  }   
+  
+  // Normallize rfrac
+  while ((rfrac & FRAC_MASK) > 0 && (rfrac & MANT_MASK) == 0) // While the MSBit is 0
+  {
+    rfrac = rfrac << 1;
+    rexp = rexp - 1;
+  }   
+  rfrac = rfrac & FRAC_MASK; // Drop the implicit 1
+
+  dandi.u =  ((rsign << SIGN_SHIFT) & SIGN_MASK) | 
+             ((rexp << EXP_SHIFT) & EXP_MASK) | 
+             ((rfrac << FRAC_SHIFT) & FRAC_MASK);
+  toReturn = dandi.d;
+  dandi2.d = toReturn;
+ // printf("x:%lu (0x%016lx)\ny:%lu (0x%016lx)\nx*y:%lu (0x%016lx)\n",xfrac,xfrac,yfrac,yfrac,rfrac,rfrac);
+ // printf("The double value %f (bit pattern 0x%02x%02x%02x%02x%02x%02x%02x%02x) represents the following:\n",
+ //         dandi2.d, dandi2.c[0], dandi2.c[1], dandi2.c[2], dandi2.c[3], 
+ //         dandi2.c[4], dandi2.c[5], dandi2.c[6], dandi2.c[7]); 
+ // printf("sign: %d (0x%lx)\texponent: %lu (0x%lx) \tmantissa: %f (0x%lx)\n", 
+ //         rsign, ((dandi2.u & SIGN_MASK) >> SIGN_SHIFT), 
+ //         (rexp-EXP_BIAS), ((dandi2.u & EXP_MASK) >> EXP_SHIFT), 
+ //         ((double)rfrac/pow(2.0,54.0)), ((dandi2.u & FRAC_MASK) >> FRAC_SHIFT));
+  
+  return toReturn;
 }
 
 double fdiv(double x, double y)
@@ -198,7 +298,17 @@ double fdiv(double x, double y)
   yfrac = ((dandi2.u & FRAC_MASK) >> FRAC_SHIFT);
   ysign = (dandi2.u & SIGN_MASK) >> SIGN_SHIFT;
 
+  if(yfrac == 0 && yexp == 0)
+  {
+    // Division by zero
+  }
+  if(xfrac == 0 && xexp == 0)
+  {
+    return 0.0; // As opposed to, you know, the 0.0 below 
+  }
+
   return 0.0;
+
 }
 
 double fsqr(double x)
@@ -211,37 +321,71 @@ void fops()
 {
   double x;
   double y;
-  x = 7.0;
+  x = 9.0;
   y = 3.0;
   printf("fadd(%f,%f) = %f\n\n",x,y,fadd(x,y));
-  x = 7.0;
+  x = 9.0;
   y = 1.0;
   printf("fadd(%f,%f) = %f\n\n",x,y,fadd(x,y));
-  x = 7.0;
+  x = 9.0;
   y = 23.0;
   printf("fadd(%f,%f) = %f\n\n",x,y,fadd(x,y));
-  x = 7.0;
+  x = 9.0;
   y = 0.0;
   printf("fadd(%f,%f) = %f\n\n",x,y,fadd(x,y));
-  x = -7.0;
+  x = -9.0;
   y = -3.0;
   printf("fadd(%f,%f) = %f\n\n",x,y,fadd(x,y));
-  x = 7.0;
+  x = 9.0;
   y = -3.0;
   printf("fadd(%f,%f) = %f\n\n",x,y,fadd(x,y));
-  x = -7.0;
+  x = -9.0;
   y = 3.0;
   printf("fadd(%f,%f) = %f\n\n",x,y,fadd(x,y));
   x = 9.0;
   y = -1.0;
   printf("fadd(%f,%f) = %f\n\n",x,y,fadd(x,y));
   x = 3.0;
-  y = -7.0;
+  y = -9.0;
   printf("fadd(%f,%f) = %f\n\n",x,y,fadd(x,y));
   x = 3.0;
-  y = -7.0;
+  y = -9.0;
   printf("fsub(%f,%f) = %f\n\n",x,y,fsub(x,y));
   x = 3.0;
-  y = 7.0;
+  y = 9.0;
   printf("fsub(%f,%f) = %f\n\n",x,y,fsub(x,y));
+  x = 1.0;
+  y = 9.0;
+  printf("fmul(%f,%f) = %f\n\n",x,y,fmul(x,y));
+  x = 9.0;
+  y = 6.0;
+  printf("fmul(%f,%f) = %f\n\n",x,y,fmul(x,y));
+  x = 3.0;
+  y = 6.0;
+  printf("fmul(%f,%f) = %f\n\n",x,y,fmul(x,y));
+  x = 1024*1024;
+  y = -(1024*1024);
+  printf("fadd(%f,%f) = %f\n\n",x,y,fadd(x,y));
+  x = 1024*1024;
+  y = -(1024*1024) + 0.5;
+  printf("fadd(%f,%f) = %f\n\n",x,y,fadd(x,y));
+  x = 1024*1024;
+  y = -(1024*1024) - .5;
+  printf("fadd(%f,%f) = %f\n\n",x,y,fadd(x,y));
+  x = 9.5*1024;
+  y = 6.0;
+  printf("fmul(%f,%f) = %f\n\n",x,y,fmul(x,y));
+  x = 3.0*128;
+  y = -6.5;
+  printf("fmul(%f,%f) = %f\n\n",x,y,fmul(x,y));
+  x = 128;
+  y = 127;
+  printf("fmul(%f,%f) = %f\n\n",x,y,fmul(x,y));
+  x = 9;
+  y = 1;
+  printf("fmul(%f,%f) = %f\n\n",x,y,fmul(x,y));
+  x = 6;
+  y = 3;
+  printf("fmul(%f,%f) = %f\n\n",x,y,fmul(x,y));
+
 }
