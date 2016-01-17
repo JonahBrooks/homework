e10a74888fc5213a891e5ad6cbf36cd130efab6a
JonahBrooks <brooks.jonah.c@gmail.com>
2016-01-17 06:55:22 +0000

    Finished cs480 hw1

--- cs480/hw1/test.c
+++ cs480/hw1/test.c
@@ -1,33 +1,10 @@
-#include <stdio.h>
 int main()
 {
-int y = 0;
-int x = 0;
-int s = 0;
-int j = 0;
-int i = 0;
-
-x = 3;
-y = 4 + x;
-x = 2 + 1;
-printf("%d\n", (-x) + y);
-i = 5;
-int i_ = 0;
-int tmp = x + y;
-for(i_ = 0; i_ < tmp; i_++)
-{
-	i = i_;
-	printf("%d %d\n", i + x, i);
-}
-s = 0;
-int j_ = 0;
-int tmp_ = 10;
-for(j_ = 0; j_ < tmp_; j_++)
-{
-	j = j_;
-	s = s + j;
-}
-printf("%d %d %d\n", s, j, y);
-
-return 0;
+  int a, b;
+  b = 1;
+  for(a = 0; a < 2; a++)
+  {
+    --b;
+  }
+  return 1;
 }
